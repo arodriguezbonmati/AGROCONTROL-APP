9d1ca039db5185cf03de180d2f26fc00426cd4b9
Mateo <33837480+mateosip@users.noreply.github.com>
2019-05-04 20:38:21 +0200

    Create compuertas.py

---
+++ compuertas.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+import cgi;
+import cgitb;
+cgitb.enable()
+print("Content-Type: text/plain")
+print("")
+form = cgi.FieldStorage()
+GPIO.setmode(GPIO.BOARD)
+led = 11
+servo = 13
+poco = 10
+GPIO.setup(led,GPIO.OUT)
+GPIO.setup(servo,GPIO.OUT)
+p = GPIO.PWM(servo,50)
+p.start(5)
+
+GPIO.output(led,True)
+p.ChangeDutyCycle(10.5)
+time.sleep(poco)
+GPIO.output(led,False)
+p.ChangeDutyCycle(5)
+time.sleep(0.5)
+p.stop()
+print 'Se ha realizado con exito'
+GPIO.cleanup()
